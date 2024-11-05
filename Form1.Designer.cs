dc3709a4baf08b9b16e8839a9c4721398dffb1bf
Semyon <cemen72421@gmail.com>
2024-11-05 14:43:06 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace AI_FileCombiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.browseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tb_FolderPath = new System.Windows.Forms.TextBox();
+            this.tb_FileFilter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Concatenate = new System.Windows.Forms.Button();
+            this.numericUpDown_CharsPerPage = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_NextPage = new System.Windows.Forms.Button();
+            this.btn_PrevPage = new System.Windows.Forms.Button();
+            this.lbl_PageInfo = new System.Windows.Forms.Label();
+            this.rtb_Output = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_CharsPerPage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseToolStripMenuItem,
+            this.copyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(994, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // browseToolStripMenuItem
+            // 
+            this.browseToolStripMenuItem.Name = "browseToolStripMenuItem";
+            this.browseToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.browseToolStripMenuItem.Text = "Browse";
+            this.browseToolStripMenuItem.Click += new System.EventHandler(this.browseToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // tb_FolderPath
+            // 
+            this.tb_FolderPath.Location = new System.Drawing.Point(3, 16);
+            this.tb_FolderPath.Name = "tb_FolderPath";
+            this.tb_FolderPath.ReadOnly = true;
+            this.tb_FolderPath.Size = new System.Drawing.Size(433, 20);
+            this.tb_FolderPath.TabIndex = 1;
+            // 
+            // tb_FileFilter
+            // 
+            this.tb_FileFilter.Location = new System.Drawing.Point(6, 68);
+            this.tb_FileFilter.Name = "tb_FileFilter";
+            this.tb_FileFilter.Size = new System.Drawing.Size(100, 20);
+            this.tb_FileFilter.TabIndex = 2;
+            this.tb_FileFilter.TextChanged += new System.EventHandler(this.tb_FileFilter_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File filter (*.cs):";
+            // 
+            // btn_Concatenate
+            // 
+            this.btn_Concatenate.Location = new System.Drawing.Point(862, 16);
+            this.btn_Concatenate.Name = "btn_Concatenate";
+            this.btn_Concatenate.Size = new System.Drawing.Size(128, 43);
+            this.btn_Concatenate.TabIndex = 5;
+            this.btn_Concatenate.Text = "Run";
+            this.btn_Concatenate.UseVisualStyleBackColor = true;
+            this.btn_Concatenate.Click += new System.EventHandler(this.btn_Concatenate_Click);
+            // 
+            // numericUpDown_CharsPerPage
+            // 
+            this.numericUpDown_CharsPerPage.Location = new System.Drawing.Point(205, 67);
+            this.numericUpDown_CharsPerPage.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown_CharsPerPage.Name = "numericUpDown_CharsPerPage";
+            this.numericUpDown_CharsPerPage.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown_CharsPerPage.TabIndex = 6;
+            this.numericUpDown_CharsPerPage.ValueChanged += new System.EventHandler(this.numericUpDown_CharsPerPage_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Chars per page::";
+            // 
+            // btn_NextPage
+            // 
+            this.btn_NextPage.Location = new System.Drawing.Point(939, 67);
+            this.btn_NextPage.Name = "btn_NextPage";
+            this.btn_NextPage.Size = new System.Drawing.Size(52, 30);
+            this.btn_NextPage.TabIndex = 8;
+            this.btn_NextPage.Text = "=>";
+            this.btn_NextPage.UseVisualStyleBackColor = true;
+            this.btn_NextPage.Click += new System.EventHandler(this.btn_NextPage_Click);
+            // 
+            // btn_PrevPage
+            // 
+            this.btn_PrevPage.Location = new System.Drawing.Point(862, 68);
+            this.btn_PrevPage.Name = "btn_PrevPage";
+            this.btn_PrevPage.Size = new System.Drawing.Size(52, 30);
+            this.btn_PrevPage.TabIndex = 9;
+            this.btn_PrevPage.Text = "<=";
+            this.btn_PrevPage.UseVisualStyleBackColor = true;
+            this.btn_PrevPage.Click += new System.EventHandler(this.btn_PrevPage_Click);
+            // 
+            // lbl_PageInfo
+            // 
+            this.lbl_PageInfo.AutoSize = true;
+            this.lbl_PageInfo.Location = new System.Drawing.Point(920, 76);
+            this.lbl_PageInfo.Name = "lbl_PageInfo";
+            this.lbl_PageInfo.Size = new System.Drawing.Size(13, 13);
+            this.lbl_PageInfo.TabIndex = 10;
+            this.lbl_PageInfo.Text = "0";
+            // 
+            // rtb_Output
+            // 
+            this.rtb_Output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb_Output.Location = new System.Drawing.Point(0, 0);
+            this.rtb_Output.Name = "rtb_Output";
+            this.rtb_Output.ReadOnly = true;
+            this.rtb_Output.Size = new System.Drawing.Size(994, 430);
+            this.rtb_Output.TabIndex = 11;
+            this.rtb_Output.Text = "";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Concatenate);
+            this.splitContainer1.Panel1.Controls.Add(this.tb_FolderPath);
+            this.splitContainer1.Panel1.Controls.Add(this.tb_FileFilter);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown_CharsPerPage);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_PageInfo);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_NextPage);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_PrevPage);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rtb_Output);
+            this.splitContainer1.Size = new System.Drawing.Size(994, 534);
+            this.splitContainer1.SplitterDistance = 100;
+            this.splitContainer1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 558);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_CharsPerPage)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem browseToolStripMenuItem;
+        private System.Windows.Forms.TextBox tb_FolderPath;
+        private System.Windows.Forms.TextBox tb_FileFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Concatenate;
+        private System.Windows.Forms.NumericUpDown numericUpDown_CharsPerPage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_NextPage;
+        private System.Windows.Forms.Button btn_PrevPage;
+        private System.Windows.Forms.Label lbl_PageInfo;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtb_Output;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI_FileCombiner
+{
+    public partial class Form1 : Form
+    {
+        private string concatenatedText;
+        private int charsPerPage = 0; // 0 means no pagination
+        private int currentPage = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    tb_FolderPath.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btn_Concatenate_Click(object sender, EventArgs e)
+        {
+            string folderPath = tb_FolderPath.Text;
+            string fileFilter = tb_FileFilter.Text;
+            charsPerPage = (int)numericUpDown_CharsPerPage.Value;
+
+
+            if (string.IsNullOrEmpty(folderPath) || !Directory.Exists(folderPath))
+            {
+                MessageBox.Show("Укажите корректный путь к папке.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                var files = Directory.GetFiles(folderPath, fileFilter, SearchOption.AllDirectories);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (string file in files)
+                {
+                    sb.Append(File.ReadAllText(file));
+                    sb.AppendLine(); // Add a newline between files
+                }
+
+                concatenatedText = sb.ToString();
+                currentPage = 1;
+                UpdateTextBox();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при чтении файлов: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateTextBox()
+        {
+            if (charsPerPage > 0 && concatenatedText.Length > charsPerPage)
+            {
+                int startIndex = (currentPage - 1) * charsPerPage;
+                int endIndex = Math.Min(startIndex + charsPerPage, concatenatedText.Length);
+                rtb_Output.Text = concatenatedText.Substring(startIndex, endIndex - startIndex);
+
+                btn_PrevPage.Enabled = currentPage > 1;
+                btn_NextPage.Enabled = endIndex < concatenatedText.Length;
+
+                lbl_PageInfo.Text = $"{currentPage}";
+                lbl_PageInfo.Visible = true; // Show page info
+
+                btn_PrevPage.Visible = true;
+                btn_NextPage.Visible = true;
+
+            }
+            else
+            {
+                rtb_Output.Text = concatenatedText;
+
+                lbl_PageInfo.Visible = false; // Hide page info if no pagination
+                btn_PrevPage.Visible = false;
+                btn_NextPage.Visible = false;
+            }
+
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(rtb_Output.Text);
+        }
+
+        private void btn_NextPage_Click(object sender, EventArgs e)
+        {
+            currentPage++;
+            UpdateTextBox();
+        }
+
+        private void btn_PrevPage_Click(object sender, EventArgs e)
+        {
+            currentPage--;
+            UpdateTextBox();
+        }
+
+        private void numericUpDown_CharsPerPage_ValueChanged(object sender, EventArgs e)
+        {
+            // Recalculate pagination if the chars per page value is changed
+            currentPage = 1;
+            UpdateTextBox();
+        }
+
+        private void tb_FileFilter_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
